4eacb5b48b1228b3e43cdaa06c0b442465d9cee6
eefesafak <79419141+eefesafak@users.noreply.github.com>
2022-03-21 17:08:26 +0300

    Add files via upload

---
+++ QTimer/mytimer.cpp
@@ -0,0 +1,14 @@
+#include "mytimer.h"
+#include <QtCore>
+#include <QDebug>
+MyTimer::MyTimer()
+{
+    timer = new QTimer(this);
+    connect(timer, SIGNAL(timeout()),this, SLOT(MySlot()));
+
+    timer -> start(1000);
+}
+
+void MyTimer::MySlot(){
+    qDebug() << "Timer Executed";
+}

cf0e13b31f8b02b079aa4f6751f7ad9093f61a35
eefesafak <79419141+eefesafak@users.noreply.github.com>
2022-03-25 16:58:48 +0300

    Add files via upload

---
+++ SmartPointers/test.cpp
@@ -0,0 +1,22 @@
+#include "test.h"
+
+Test::Test(QObject *parent)
+    : QObject{parent}
+{
+    qInfo() << this << "Constructed" << parent;
+}
+
+Test::~Test()
+{
+    foreach(QObject* child, children()){
+        qInfo() << this << " - child - " << child;
+    }
+
+    qInfo() << this << "Deconstructed" << parent();
+}
+
+void Test::message(QString value)
+{
+    qInfo() << this << value;
+
+}
